2dcb40db7248bbbf11603fcb4d50a345ba2f8201
Jack Dempsey <thrawn.chimera@gmail.com>
2014-02-19 20:26:23 -0800

    updates to assembler

--- cis330-project/src/Assembler.hpp
+++ cis330-project/src/Assembler.hpp
@@ -1,32 +1,43 @@
 //
 //  Assembler.hpp
 //  
-//
-//  Created by Jack Dempsey on 2/16/14.
-//
-//
 
 #ifndef ____Assembler__
 #define ____Assembler__
 
 #include <iostream>
 #include <std.bool.h>
-class ASsembler {
+class Assembler {
 public:
     Assembler::Assembler();
     Assembler::~Assember();
-    void Assembler::splitInput(const std::string &inputString, std::vector<std::string> &elems);
-    void Assembler::getOperation(std::string operationString);
-    void Assembler::getRegisters(Std::string registerString);
-
+    void Assembler::getInput();
+    void Assembler::splitInput(const std::string &inputString, std::vector<const std::string> &elems);
+    void Assembler::getOperation(const std::string &operationString);
+    void Assembler::getDestRegister(const std::string &registerString);
+    void Assembler::getSrcRegister(const std::string &registerString);
+    void Assembler::getAddress(const std::string &addrString);
+    int Assembler::Assemble();
 private:
-    int op[5];
+    // The original instruction string input to the assembler
+    std:string input;
+    // A flag flipped if the instruction include an address of constant value
+    int isAddr;
+    // The decimal representation of the object code
+    int object_code;
+    // The decimal value of the operation code
+    int opcode;
+    // The decimal value of the immediate value
     int immediate;
+    // How many registers are used in the instruction
     int numRegisters;
-    std::bool isAddr;
-    int reg1;
-    int reg2;
+    // The decimal value of the destination register
+    int destReg;
+    // The decimal value of the source register
+    int srcReg;
+    // The decimal value of the constant or address
     int addr;
-    vector<std::string> instructParts;
+    // A vector to hold different parts of the input string
+    vector<const std::string> instructParts;
 };
 #endif /* defined(____Assembler__) */
